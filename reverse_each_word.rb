ff03369ce88dc94b542c32533d170c9bfa30204c
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-17 16:13:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,16 +2,9 @@ def reverse_each_word(str)
   array_to_return = []
   str_to_array = str.split
   array_to_return.push(str_to_array.collect{|element| element.reverse})
-  str_to_return = array_to_return * " "
-  str_to_return
+  array_to_return * " "
+  
 end
 
-our_str = "this 31 the string"
-puts our_str
-
-array = our_str.split
-
-aaaa = reverse_each_word(our_str)
-str = aaaa*" "
-
-p str
+our_array = %w[this is an example]
+p our_array
