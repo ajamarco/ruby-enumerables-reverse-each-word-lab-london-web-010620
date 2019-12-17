f7f37a0513c780e51cdd19ab0a116626ea5897af
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-17 16:11:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(str)
   array_to_return = []
   str_to_array = str.split
   array_to_return.push(str_to_array.collect{|element| element.reverse})
-  array_to_return
+  array_to_return * " "
 end
 
 our_str = "this 31 the string"
